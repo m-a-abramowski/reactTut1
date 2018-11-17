e0573ef6079b52ae5e2fa6348ef95b089cad0919
m-a-abramowski <m.a.abramowski@gmail.com>
2018-11-17 01:39:17 +0100

    2nd temp

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,55 @@
 console.log('App.js is running.');
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ul',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var newtemp = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Maciek'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 29'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Warsaw'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(newtemp, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is running.');
 
-var template = <h1>Indecision</h1>;
+var template = (
+<div>
+  <h1>Indecision</h1>
+  <p>This is some info</p>
+  <ul>
+    <li>Item one</li>
+    <li>Item two</li>
+  </ul>
+</div>
+);
+
+var newtemp = (
+  <div>
+    <h1>Maciek</h1>
+    <p>Age: 29</p>
+    <p>Location: Warsaw</p>
+  </div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(newtemp, appRoot);
