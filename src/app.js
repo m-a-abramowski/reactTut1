a73bd024093434a121da3522deb55c3d74ef301b
m-a-abramowski <m.a.abramowski@gmail.com>
2018-11-17 01:22:46 +0100

    dependencies

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-'use strict';
-
 console.log('App.js is running.');
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision'
-);
+var template = <h1>Indecision</h1>;
 
 var appRoot = document.getElementById('app');
 
